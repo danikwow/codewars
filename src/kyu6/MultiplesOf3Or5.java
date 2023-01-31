caaf12341f93e845dc92cd24d86f0b8316eb2126
danikwow <122081538+danikwow@users.noreply.github.com>
2023-01-31 23:35:38 +0700

    Complete Kata

---
+++ src/kyu6/MultiplesOf3Or5.java
@@ -0,0 +1,21 @@
+package kyu6;
+
+public class MultiplesOf3Or5 {
+    public int solution(int number) {
+        if (number < 0) return 0;
+        int[] num = new int[number];
+        int counter = 0;
+        int result = 0;
+        for (int i = 0; i < number; i++){
+            counter++;
+            num[i] = counter;
+        }
+        for (int i = 0; i < number - 1; i++){
+            if (num[i] == 1) continue;
+            if (num[i]%3 == 0 || num[i]%5 == 0){
+                result += num[i];
+            }
+        }
+        return result;
+    }
+}
