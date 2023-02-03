08d3129d74d6a2f74f20e4b9151c3b72a408cabe
danikwow <122081538+danikwow@users.noreply.github.com>
2023-02-03 22:16:01 +0700

    Add other version.

--- src/kyu6/TenMinWalk.java
+++ src/kyu6/TenMinWalk.java
@@ -16,3 +16,21 @@ public class TenMinWalk {
         return (x == 0 && y == 0 && count == 10);
     }
 }
+/*
+
+import java.awt.Point;
+
+public class TenMinWalk {
+    public static boolean isValid(char[] walk) {
+        Point point = new Point(0,0);
+        for (char c : walk) {
+            switch (c) {
+                case 'n': point.translate(1,0); break;
+                case 'e': point.translate(0,1); break;
+                case 's': point.translate(-1,0); break;
+                case 'w': point.translate(0,-1); break;
+            }
+        }
+        return point.equals(new Point(0,0)) && walk.length == 10;
+    }
+}*/
