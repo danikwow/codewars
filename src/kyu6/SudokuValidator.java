defd0e40b9abf6b5041583c0ffcd8497cd78d4a8
danikwow <122081538+danikwow@users.noreply.github.com>
2023-02-04 16:32:06 +0700

    Add 2 other Version.

--- src/kyu6/SudokuValidator.java
+++ src/kyu6/SudokuValidator.java
@@ -62,3 +62,56 @@ public class SudokuValidator {
         return true;
     }
 }
+
+/* Other Version
+public class SudokuValidator {
+
+    public static boolean validate(int[][] x) {
+        int pri [] = {0,2,3,5,7,11,13,17,19,23};
+        int row [] = new int [9]; one(row);
+        int col [] = new int [9]; one(col);
+        int cen [] = new int [9]; one(cen);
+        for (int i=0; i<9 ;i++){
+            for (int j=0; j<9; j++){
+                int c = pri[x[i][j]]; if(c==0)return false;
+                int index=(i/3)*3+j/3;
+                row[i]*=c; col[j]*=c; cen[index]*=c; c=c*c;
+                if((row[i]%c)*(col[j]%c)*(cen[index]%c)==0) return false;
+            }
+        }
+        return true;
+    }
+    public static void one (int x [] ) {
+        for (int i=0 ;i<9; i++) x[i]=1;
+    }
+
+}*/
+
+/* Other Version
+import java.util.function.BiFunction;
+        import java.util.stream.*;
+        import java.util.*;
+
+
+public class SudokuValidator {
+
+    private static Set<Integer> BASE = Set.of(1,2,3,4,5,6,7,8,9);
+
+    public static boolean validate(int[][] board) {
+
+        BiFunction<Integer,Integer,Integer>
+                rows  = (i,j) -> board[i][j],
+                cols  = (i,j) -> board[j][i],
+                boxes = (i,j) -> board[ i/3*3 + j/3 ][ i%3*3 + j%3 ];
+
+        return check(rows) && check(cols) && check(boxes);
+    }
+
+    private static boolean check(BiFunction<Integer,Integer,Integer> extract){
+        return IntStream.range(0,9).boxed()
+                .map( i -> IntStream.range(0,9).boxed()
+                        .map(j -> extract.apply(i,j) )
+                        .collect(Collectors.toSet()) )
+                .allMatch( set -> set.equals(BASE) );
+    }
+}*/
