1ca95d37a6506eda73f3c401a3127fa1e493ac96
danikwow <122081538+danikwow@users.noreply.github.com>
2023-02-12 17:01:45 +0700

    Complete Kata.

---
+++ src/kyu6/StringSplit.java
@@ -0,0 +1,24 @@
+package kyu6;
+
+public class StringSplit {
+    public static String[] solution(String s) {
+        String[] result = {};
+        int count = 0;
+        if (s.length() % 2 == 0) {
+            result = new String[s.length() / 2];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = ("" + s.charAt(count) + s.charAt(count + 1));
+                count+= 2;
+            }
+        }
+        if (s.length() % 2 != 0) {
+            result = new String[(s.length() / 2) + 1];
+            for (int i = 0; i < result.length - 1; i++) {
+                result[i] = ("" + s.charAt(count) + s.charAt(count + 1));
+                count+= 2;
+            }
+            result[result.length - 1] = ("" + s.charAt(s.length() - 1) + "_");
+        }
+        return result;
+    }
+}
